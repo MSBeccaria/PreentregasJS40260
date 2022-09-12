666f1e0383bdd5c10dfa9375802822ad8d81a313
Soledad Beccaria <mariasoledadbeccaria@gmail.com>
2022-09-12 11:17:02 -0300

    arreglo

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/*creo un array con las opciones de tours*/
+/*creo un array con las opciones de tours
 
 
 const citytour = [
@@ -8,10 +8,42 @@ const citytour = [
 
 ];
 
-/*uso find para que el usuario elija el tour y sepa que días se hace*/
+/*uso find para que el usuario elija el tour y sepa que días se hace
 
 let nombre = prompt("Qué citytour quieres hacer?");
 let tour = citytour.find(item => item.nombre === nombre);
 let mensaje = `El citytour ${nombre} se realiza los días ${tour.turno}`;
-alert(mensaje);
+alert(mensaje);*/
 
+
+/*esto como para recopilar datos de los inscriptos, pero no se si está bien*/
+
+class Turista{
+    constructor(tour, nombre, apellido, mail) {
+        this.tour = tour;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.mail = mail;
+    
+    }
+}
+
+const turistas = [];
+turistas.push(new Turista("Casco histórico", "Susana", "Lopez", "susana@lopez"));
+turistas.push(new Turista("Patrimonio arquitectura", "Juan", "Perez", "juan@perez"));
+turistas.push(new Turista("Casco histórico", "Ana", "Gomez", "ana@gomez"));
+console.log(turistas);
+
+
+
+function buscarTurista(tour, apellido){
+    return tour.find( objeto => objeto.nombre === apellido.tuUpperCase());
+}
+for (let index=0; index<3; index ++){
+    let busqueda = buscarTurista(turistas,prompt('Ingrese Apellido del turista'));
+    if (busqueda != undefined){
+    alert ('Reserva de'+ busqueda.apellido + 'para tour' + busqueda.tour);
+} else{
+    alert ('No hay reserva a ese nombre')
+}
+}
